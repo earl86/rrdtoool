2aafac8e56e5f461c50bddde7723b34f406a9e52
earl86 <earl86@users.noreply.github.com>
2018-05-17 11:38:19 +0800

    Add files via upload

---
+++ update.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+import rrdtool
+import time
+import psutil
+
+
+total_input_traffic = psutil.net_io_counters()[1]
+total_output_traffic = psutil.net_io_counters()[0]
+starttime = int(time.time())
+update = rrdtool.updatev('/root/test/rrdtool/Flow.rrd', '%s:%s:%s' %(str(starttime), str(total_input_traffic), str(total_output_traffic)))
+print update
+
